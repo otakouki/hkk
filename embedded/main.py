6f0547a0f9e196b556139d74306284cbd1395420
unknown <2190347@ecccomp.com>
2021-12-22 09:10:19 +0900

    軽量化

--- embedded/main.py
+++ embedded/main.py
@@ -1,31 +1,51 @@
+import sys
+sys.dont_write_bytecode = True # キャッシュ無効化
 import threading
 import numpy as np
 import cv2
 import camerathreading
 import mtcnnthreading
 
-#cv2.setNumThreads(0)
+# OpenCV Bug Fix
+cv2.setNumThreads(0)
 
 def main():
-    cap = camerathreading.CameraThreading(0)
+    try:
+        # カメラ
+        cap = camerathreading.CameraThreading(0)
 
-    if not cap.isOpened():
-        raise RuntimeError
+        if not cap.isOpened():
+            raise RuntimeError
 
-    cap.start()
+        cap.start()
+        mtcnn = mtcnnthreading.MTCNNThreading(cap)
+        mtcnn.start()
 
-    mtcnn = mtcnnthreading.MTCNNThreading(cap)
-    mtcnn.start()
+        while True:
+            # エラー処理
+            if not cap.isOpened():
+                raise RuntimeError
 
-    while True:
-        _, frame = cap.read()
+            # カメラフレーム
+            _, frame = cap.read()
 
-        if cv2.waitKey(1) & 0xff == ord('q'):
-            break
+            # 顔認識結果表示
+            print(mtcnn.read())
 
-    mtcnn.release()
-    cap.release()
-    cv2.destroyAllWindows()
+            # qキーで終了
+            if cv2.waitKey(1) & 0xff == ord('q'):
+                break
+
+        # 解放処理
+        mtcnn.release()
+        cap.release()
+        cv2.destroyAllWindows()
+
+    except RuntimeError:
+        print("カメラが見つかりません。カメラの接続を確認してください。")
+        sys.exit()
+    except KeyboardInterrupt:
+        sys.exit()
 
 if __name__ == '__main__':
 	main()
