46c8e8baa0dfd8335cab7e6fb7beae870ead90fb
otakouki <koki.oot@gmail.com>
2021-12-22 09:39:26 +0900

    dellete shell script

--- opencv.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-sudo rpi-eeprom-update
-
-sudo rpi-eeprom-update -a
-
-wget https://github.com/Qengineering/Install-OpenCV-Raspberry-Pi-32-bits/raw/main/OpenCV-4-5-4.sh
-sudo chmod 755 ./OpenCV-4-5-4.sh
-./OpenCV-4-5-4.sh 
